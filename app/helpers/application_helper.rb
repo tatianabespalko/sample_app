5f1e1f70140a2b4056af580493f4732c5b567b9d
TBespalko <tbespalko7@gmail.com>
2017-11-20 22:52:18 +0200

    modified chapter_3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis. # Документирующий комментарий
+  def full_title(page_title)   # Определение метода
+    base_title = "Ruby on Rails Tutorial Sample App"  # Назначение переменной
+    if page_title.empty?        # Булевый тест
+      base_title                # Явное возвращение
+    else
+      "#{base_title} | #{page_title}"    # Интерполяция строки
+    end
+  end
 end
