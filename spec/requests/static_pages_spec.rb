2e4c6646c90a5111a19ce3d9a0777471a736ec37
TBespalko <tbespalko7@gmail.com>
2017-12-06 23:33:36 +0200

    Ready to check ex. 5.6.2-5.6.4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,7 @@ require 'spec_helper'
 		shared_examples_for "all static pages" do
 			it { should have_selector('h1', text: heading) }
 	    	it { should have_title(full_title(page_title)) }
+
 		end
 		describe "Home page" do
 			  
@@ -48,9 +49,33 @@ require 'spec_helper'
 			let(:heading)    { 'Contact' }
 		    let(:page_title) { '' }	
 		    it_should_behave_like "all static pages"		
-#		    it { should have_content('Contact') }
+		    it { should have_content('Contact') }
 #		    it { should have_selector('h1', text: 'Contact') }
-		    it { should have_title(full_title('Contact')) }		    
+#		    it { should have_title(full_title('Contact')) }		    
 	    
 		end
 end
+
+
+#require 'spec_helper'
+
+#describe "Static pages" do
+#  .
+#  .
+#  .
+#  it "should have the right links on the layout" do
+#    visit root_path
+#    click_link "About"
+#    expect(page).to have_title(full_title('About Us'))
+#    click_link "Help"
+#    expect(page).to # заполнить expect(page).to have_title(full_title('Help')
+#    click_link "Contact"
+#    expect(page).to # заполнить expect(page).to have_title(full_title('Contact')
+#    click_link "Home"
+#    click_link "Sign up now!"
+#    expect(page).to # заполнить  expect(page).to have_title(full_title('Sign up now!')
+#    click_link "sample app"
+#    expect(page).to # заполнить  expect(page).to have_content('Sample App')
+#  end
+#end
+
