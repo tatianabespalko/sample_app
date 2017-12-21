1b2fe8c3b0854f71b415116c9ff62e923a1e8421
TBespalko <tbespalko7@gmail.com>
2017-12-22 01:10:58 +0200

    update chapter 8

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,20 +1,13 @@
 include ApplicationHelper
 
-include SessionsHelper
-
-#def full_title(page_title)
-#  base_title = "Ruby on Rails Tutorial Sample App"
-#  if page_title.empty?
-#    base_title
-#  else
-#    "#{base_title} | #{page_title}"
-#  end
-#end
-
-def sign_in(user)
-  click_button "Sign in"
-  fill_in "Email",        with: user.email
-  fill_in "Password",     with: user.password
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
   click_button "Sign in"
 end
 
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
